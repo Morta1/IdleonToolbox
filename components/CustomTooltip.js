10dcfb627a1981f74b8a9ec7e38a5edbf1615240
Mor <mortaob@gmail.com>
2021-08-05 10:41:48 +0300

    Fixed card stats

--- components/CustomTooltip.js
+++ components/CustomTooltip.js
@@ -2,28 +2,31 @@ import styled from "styled-components";
 import { Tooltip } from "@material-ui/core";
 import { prefix } from "../Utilities";
 
-const CustomTooltip = ({ header, stats, children }) => {
+const CustomTooltip = ({ header, base, children }) => {
   return (
     <StyledTooltip
+      enterTouchDelay={0}
       placement={"top"}
       title={
         <div className="tooltip-body">
           <div className="tooltip-header">{header}</div>
           <div className="stars">
-            {(stats || [1, 2, 3, 4]).map((stat, index) => {
-              return (
-                <div className="star-line" key={stat + " " + index}>
-                  <div className="image-wrapper">
-                    {index === 0 ? (
-                      "Base"
-                    ) : (
-                      <img src={`${prefix}/Star${index}.png`} alt="" />
-                    )}
-                  </div>
-                  <div className="stat">{stat}</div>
-                </div>
-              );
-            })}
+            {base
+              ? [1, 2, 3, 4]?.map((_, index) => {
+                  return (
+                    <div className="star-line" key={base + " " + index}>
+                      <div className="image-wrapper">
+                        {index === 0 ? (
+                          <span style={{ fontWeight: "bold" }}>Base</span>
+                        ) : (
+                          <img src={`${prefix}/Star${index}.png`} alt="" />
+                        )}
+                      </div>
+                      <div className="stat">{base * (index + 1)}</div>
+                    </div>
+                  );
+                })
+              : null}
           </div>
         </div>
       }
@@ -42,24 +45,33 @@ const StyledTooltip = styled((props) => (
   & .tooltip {
     font-size: 16px;
     background-color: rebeccapurple;
-    min-height: 50px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
 
   .tooltip-body {
     .tooltip-header {
+      text-align: center;
       font-weight: bold;
+      padding: 10px 0;
     }
 
     .stars {
+      border-top: 1px solid #51325a;
+      padding: 10px 0;
+      display: flex;
+      justify-content: space-around;
+
       .star-line {
         display: flex;
         align-items: center;
+        flex-direction: column;
+        margin: 0 10px;
       }
       .image-wrapper {
-        min-width: 40px;
+        /* min-width: 40px; */
       }
       .stat {
-        margin-left: 15px;
+        /* margin-left: 15px; */
       }
     }
   }
