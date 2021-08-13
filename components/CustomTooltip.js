8475d75414eae105de051462fb03464daffc3018
Mor <mortaob@gmail.com>
2021-08-13 20:10:39 +0300

    FIXED THE DAMN THING

--- components/CustomTooltip.js
+++ components/CustomTooltip.js
@@ -9,52 +9,35 @@ const CustomTooltip = ({ header, base, children }) => {
       interactive
       enterTouchDelay={100}
       placement={"top-start"}
-      title={"TEST"}
-      // title={
-      //   <div className="tooltip-body">
-      //     <div className="tooltip-header">{header}</div>
-      //     <div className="stars">
-      //       {base
-      //         ? [1, 2, 3, 4]?.map((_, index) => {
-      //           return (
-      //             <div className="star-line" key={base + " " + index}>
-      //               <div className="image-wrapper">
-      //                 {index === 0 ? (
-      //                   <span style={{ fontWeight: "bold" }}>Base</span>
-      //                 ) : (
-      //                   <img src={`${prefix}etc/Star${index}.png`} alt=""/>
-      //                 )}
-      //               </div>
-      //               <div className="stat">{base * (index + 1)}</div>
-      //             </div>
-      //           );
-      //         })
-      //         : null}
-      //     </div>
-      //   </div>
-      // }
+      title={
+        <div className="tooltip-body">
+          <div className="tooltip-header">{header}</div>
+          <div className="stars">
+            {base
+              ? [1, 2, 3, 4]?.map((_, index) => {
+                return (
+                  <div className="star-line" key={base + " " + index}>
+                    <div className="image-wrapper">
+                      {index === 0 ? (
+                        <span style={{ fontWeight: "bold" }}>Base</span>
+                      ) : (
+                        <img src={`${prefix}etc/Star${index}.png`} alt=""/>
+                      )}
+                    </div>
+                    <div className="stat">{base * (index + 1)}</div>
+                  </div>
+                );
+              })
+              : null}
+          </div>
+        </div>
+      }
     >
       {children}
     </StyledTooltip>
   );
 };
 
-// const StyledTooltip = styled(((props) =>
-//     <Tooltip {...props} classes={{ popper: props.className, tooltip: "tooltip"}}  />
-// ))`
-//   & .tooltip {
-//     color: black;
-//     font-size: 16px;
-//     background-color: #f5f5f9;
-//     border: 1px solid #dadde9;
-//     box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
-//     max-width: 300px;
-//     @media only screen and (max-width: 600px) {
-//       max-width: 200px;
-//     }
-//   }
-// `;
-//
 const StyledTooltip = styled(((props) =>
   <Tooltip {...props} classes={{ popper: props.className, tooltip: "tooltip", touch: "touch" }}  />
 ))`
