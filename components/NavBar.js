cac129ed32b5de45af0f7ca75077076c3ba301c8
Mor <mortaob@gmail.com>
2021-10-06 16:34:38 +0300

    Removed printing version from NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import { extVersion, prefix, screens } from "../Utilities";
@@ -8,10 +8,6 @@ import { AppContext } from "./context";
 const NavBar = () => {
   const { display, setUserDisplay, userData } = useContext(AppContext);
   const router = useRouter();
-
-  useEffect(() => {
-    console.log(userData?.version)
-  }, []);
 
   const appRoutes = [
     { label: "Card Search", path: prefix ? prefix : "/", name: '/' },
