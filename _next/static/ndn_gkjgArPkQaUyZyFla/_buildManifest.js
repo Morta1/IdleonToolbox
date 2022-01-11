876161714081c880b86645aac9693bac3264fee3
Morta1 <Morta1@users.noreply.github.com>
2022-01-11 13:43:34 +0000

    deploy: add526584cf478dc059726ab4a965e7550e627ae

---
+++ _next/static/ndn_gkjgArPkQaUyZyFla/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/987-f69f37a2706335642e96.js","static/chunks/pages/index-9e8f986cad15d60950c1.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
