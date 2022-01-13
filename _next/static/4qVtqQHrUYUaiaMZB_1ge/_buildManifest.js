4435d0c35f390586ca56bf2a5c95683ea7f9d2fc
Morta1 <Morta1@users.noreply.github.com>
2022-01-13 13:30:22 +0000

    deploy: 3ef600b742cc119c9fd12da0d859b6c758955c4c

--- _next/static/4qVtqQHrUYUaiaMZB_1ge/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/376-dd07596984105575f0d2.js","static/chunks/pages/index-7e0f886e7d51c196706e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
