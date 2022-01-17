a0d3745d07d476f7e09b998fc276fdb3ef8c2145
Morta1 <Morta1@users.noreply.github.com>
2022-01-17 08:15:59 +0000

    deploy: 3936550ec1d4a45e9aa63767c8c94bea41765e57

---
+++ _next/static/35gDLW5i3ZBEjPNVFr_LP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-6137caea46b1294951f0.js","static/chunks/pages/index-647c1692f9702e888d6a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Y-4W_QdfAkiz008i__dk7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-6137caea46b1294951f0.js","static/chunks/pages/index-64cf83bc1f852bde3bfd.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
