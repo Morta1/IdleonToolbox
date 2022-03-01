df2382797788e31b0b809682181ec8d9be608d1a
Morta1 <Morta1@users.noreply.github.com>
2022-03-01 09:09:30 +0000

    deploy: 6c57d223329218986e1d3f04b92121d1d0fdbe44

--- _next/static/DDEGL-ZoswdUeRLNdmp8n/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/116-852c18aacb0bbbf9.js","static/chunks/pages/index-1d2edb8cebb9299d.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
