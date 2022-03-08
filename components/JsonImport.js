37e96938f1b9f859227262fe769df02b80d0e4ce
Mor <mortaob@gmail.com>
2022-03-08 18:56:06 +0200

    Reverted paste button to accept steam data.

--- components/JsonImport.js
+++ components/JsonImport.js
@@ -143,8 +143,8 @@ const JsonImport = () => {
       });
     }
     try {
-      const { data, userNames, guildData } = JSON.parse(await navigator.clipboard.readText());
-      const parsedData = parseIdleonData(data, userNames, guildData);
+      const data = JSON.parse(await navigator.clipboard.readText());
+      const parsedData = parseIdleonData(data);
       setUserData(parsedData);
       setUserLastUpdated(getDate());
       setManualResult(true);
