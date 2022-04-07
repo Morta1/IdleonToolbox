e3361935a7802ce3d9c0d5b9e6778253ae300585
Mor <mortaob@gmail.com>
2022-04-07 19:52:10 +0300

    Added styling for arena bonuses.

--- components/Breeding/ArenaBonuses.js
+++ components/Breeding/ArenaBonuses.js
@@ -4,21 +4,37 @@ import { cleanUnderscore, pascalCase } from "../../Utilities";
 const ArenaBonuses = ({ maxArenaLevel, bonuses }) => {
   return (
     <ArenaBonusesStyle>
-      <div>Max Arena Wave: <span className={'bold'}>{maxArenaLevel}</span></div>
-      {bonuses?.map(({ bonus, wave }, index) => {
-        return <div className={maxArenaLevel < wave ? 'locked' : ''} key={`${wave}-${index}`}>
-          <div>Wave {wave}</div>
-          <div>{cleanUnderscore(pascalCase(bonus))}</div>
-        </div>
-      })}
+      <div className={'max-arena'}>Max Arena Wave: <span className={'bold'}>{maxArenaLevel}</span></div>
+      <div className="bonuses">
+        {bonuses?.map(({ bonus, wave }, index) => {
+          return <div className={`bonus${maxArenaLevel < wave ? ' locked' : ''}`} key={`${wave}-${index}`}>
+            <div>Wave {wave}</div>
+            <div>{cleanUnderscore(pascalCase(bonus))}</div>
+          </div>
+        })}
+      </div>
     </ArenaBonusesStyle>
   );
 };
 
 const ArenaBonusesStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
+  .max-arena {
+    color: #47d947;
+    text-align: center;
+    margin-bottom: 50px;
+  }
+
+  .bonuses {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    max-width: 1000px;
+  }
+
+  .bonus {
+    width: 300px;
+  }
 
   .bold {
     font-weight: bold;
