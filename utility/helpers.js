98939cf09e1c999826b25aebd7aaf25f91579a79
Mor <mortaob@gmail.com>
2022-05-17 23:14:46 +0300

    Removed redundant prefix

--- utility/helpers.js
+++ utility/helpers.js
@@ -187,4 +187,4 @@ export const splitTime = (numberOfHours) => {
 export const sections = [{ name: "Activity" },{ name: "Stats" }, { name: "Bags" }, { name: "Obols" }, { name: "Cards" }, { name: "Skills" }, { name: "Prayers" }, { name: "Talents" }, { name: "Equipment" }, { name: "Star Signs" }, { name: "Post Office" }, { name: "Anvil Details" }, { name: "Equipped Bubbles" }, { name: "Active Skills CD" }];
 
 const isProd = process.env.NODE_ENV === "production";
-export const prefix = isProd ? "/IdleonToolbox/" : "/";
+export const prefix = isProd ? "/" : "/";
