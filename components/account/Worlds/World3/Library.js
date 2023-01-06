0235634ed7b1a83dd0efb34b6deaa381a3121014
Mor <mortaob@gmail.com>
2023-01-07 00:21:26 +0200

    fix: negative time for library

--- components/account/Worlds/World3/Library.js
+++ components/account/Worlds/World3/Library.js
@@ -4,23 +4,27 @@ import { Divider, Stack, Typography } from "@mui/material";
 import { prefix } from "../../../../utility/helpers";
 
 const Library = ({ libraryTimes, lastUpdated }) => {
-  const { bookCount, next, breakpoints } = libraryTimes || {};
+  let { bookCount, next, breakpoints } = libraryTimes || {};
   return <>
     <Stack direction={'row'} alignItems={'center'} gap={1}>
       <img src={`${prefix}data/Libz.png`} alt=""/>
       <h3>Library</h3>
     </Stack>
     <h4>Book count: {bookCount}</h4>
-    <Stack direction={'row'} gap={1}>
-      <Typography sx={{ width: 100 }}>Next book in: </Typography> <Timer type={'countdown'} lastUpdated={lastUpdated}
-                                                                         date={new Date().getTime() + next * 1000}/>
+    <Stack direction={'row'} gap={1} alignItems={'center'}>
+      <Typography sx={{ width: 100 }}>Next book in: </Typography> {next > 0 ?
+      <Timer type={'countdown'} lastUpdated={lastUpdated}
+             date={new Date().getTime() + next * 1000}/> :
+      <Typography variant={'caption'}>Wait for game update</Typography>}
     </Stack>
     {breakpoints?.map(({ breakpoint, time }, index) => {
       return time > 0 ? <React.Fragment key={'book-timer' + index}>
         <Divider sx={{ my: 1 }}/>
         <Stack direction={'row'} gap={1}>
-          <Typography sx={{ width: 100 }}>{breakpoint} books in: </Typography> <Timer type={'countdown'} lastUpdated={lastUpdated}
-                                                                  date={new Date().getTime() + time * 1000}/>
+          <Typography sx={{ width: 100 }}>{breakpoint} books in: </Typography> {time > 0 ? <Timer type={'countdown'}
+                                                                                                  lastUpdated={lastUpdated}
+                                                                                                  date={new Date().getTime() + time * 1000}/> :
+          <Typography variant={'caption'}>Wait for game update</Typography>}
         </Stack>
       </React.Fragment> : null
     })}
