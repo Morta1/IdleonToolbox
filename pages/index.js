62056b5bb0b27aad36f65d118367d7ae8fc0137a
Mor <mortaob@gmail.com>
2023-04-02 22:11:50 +0300

    chore: add small feature text

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const patchNotes = [
     gameVer: "1.79",
     date: "02/04/2023",
     features: [<Typography>Updated logic for <Link
-      href={'https://idleontoolbox.com/tools/item-planner'}>Item Planner</Link> page!</Typography>],
+      href={'https://idleontoolbox.com/tools/item-planner'}>Item Planner</Link> page!</Typography>, 'Added names to sections and made them collapsable'],
     fixes: ['Fix bug with max traps in dashboard'],
     deprecatedFeatures: []
   },
