3e01da0c8303f6a32afc0320419db75d2822cca9
Mor <mortaob@gmail.com>
2023-06-04 17:49:30 +0300

    testing

--- pages/index.js
+++ pages/index.js
@@ -1197,7 +1197,7 @@ const Home = () => {
               {index % 3 === 0 ? <>
                 <Stack>
                   <Adsense
-                    style={{ display: 'inline-block', maxHeight: 90 }}
+                    style={{ display: 'inline-block', height: 90, maxHeight: 90 }}
                     client="ca-pub-1842647313167572"
                     slot="7203005854"
                     format={''}
