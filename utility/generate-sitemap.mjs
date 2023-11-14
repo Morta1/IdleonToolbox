1a949338207f46e56bfb4256f7d5e301fd0b1ea8
Mor <mortaob@gmail.com>
2023-11-14 07:41:28 +0200

    fix: hopefully better sitemap generation

---
+++ utility/generate-sitemap.mjs
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import {globby} from 'globby'
+function addPage(page) {
+  const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '')
+  const route = path === '/index' ? '' : path
+
+  return `  <url>
+    <loc>${`https://www.idleontoolbox.com${route}`}</loc>
+    <changefreq>hourly</changefreq>
+  </url>`
+}
+
+async function generateSitemap() {
+  // Ignore Next.js specific files (e.g., _app.js) and API routes.
+  const pages = await globby([
+    'pages/**/*{.js,.mdx}',
+    '!pages/_*.js',
+    '!pages/api',
+  ])
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(addPage).join('\n')}
+</urlset>`
+
+  fs.writeFileSync('public/sitemap.xml', sitemap)
+}
+
+console.log('starting sitemap generation')
+generateSitemap()
+console.log('finished sitemap generation')
