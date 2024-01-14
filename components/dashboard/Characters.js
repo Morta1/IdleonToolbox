0d5970d62bf0a17f1ccc2c92b804fe58ba601d3c
Mor <mortaob@gmail.com>
2024-01-14 20:42:24 +0200

    fix: grammar

--- components/dashboard/Characters.js
+++ components/dashboard/Characters.js
@@ -112,7 +112,7 @@ const Characters = ({ characters = [], account, lastUpdated, trackers }) => {
                 <Alert title={`${name} is missing ${alerts?.anvil?.missingHammers} hammers`}
                        iconPath={'data/GemP1'}/> : null}
               {trackers?.anvil && alerts?.anvil?.unspentPoints > 0 ?
-                <Alert title={`${name} have ${alerts?.anvil?.unspentPoints} unspent points anvil points`}
+                <Alert title={`${name} has ${alerts?.anvil?.unspentPoints} unspent points anvil points`}
                        iconPath={'data/ClassIcons43'}/> : null}
               {trackers?.anvil && alerts?.anvil?.anvilOverdue?.length > 0 ?
                 alerts?.anvil?.anvilOverdue?.map(({ diff, name, rawName }) => {
