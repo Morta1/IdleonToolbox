7161fc4a7f5b8cf36fbc165b7d3c2d3c80cc47d2
Mor <mortaob@gmail.com>
2024-02-28 21:01:39 +0200

    fix: parsing ninja upgrades

--- components/account/Worlds/World6/sneaking/Upgrades.jsx
+++ components/account/Worlds/World6/sneaking/Upgrades.jsx
@@ -1,16 +1,17 @@
 import { Card, CardContent, Stack, Typography } from '@mui/material';
-import { cleanUnderscore, prefix } from '@utility/helpers';
+import { cleanUnderscore, notateNumber, prefix } from '@utility/helpers';
 
 const Upgrades = ({ upgrades }) => {
   return <Stack direction={'row'} flexWrap={'wrap'} gap={1}>
     {upgrades?.map(({ rawName, level, name, description, modifier }, index) => {
+      const bonus = description?.includes('{') ? level * modifier : notateNumber(1 + level * modifier / 100, 'MultiplierInfo')
       return name !== 'Name' ? <Card key={'upgrade-' + index} sx={{ width: 220 }}>
         <CardContent>
           <Stack direction={'row'} gap={1}>
             <img width={32} src={`${prefix}data/NjUpgI${index}.png`} alt={''}/>
             <Typography>{cleanUnderscore(name)}</Typography>
           </Stack>
-          <Typography>{cleanUnderscore(description.replace(/{/g, level * modifier))}</Typography>
+          <Typography>{cleanUnderscore(description.replace(/}|{/g, bonus))}</Typography>
         </CardContent>
       </Card> : null
     })}
