f26daa5fa028058b86781a9b4298ccb5581a2ec5
Mor <mortaob@gmail.com>
2024-06-13 09:12:53 +0300

    fix: last feather

--- parsers/world-1/owl.js
+++ parsers/world-1/owl.js
@@ -95,11 +95,10 @@ const parseOwl = (account) => {
       bonus: account?.accountOptions?.[264]
     }
   ];
-  console.log('account?.accountOptions?.[262]', account?.accountOptions?.[262])
   const megaFeathers = megaFeathersDesc.map((description, index) => ({
     description,
     unlocked: index + 1 <= account?.accountOptions?.[262],
-    ...(index === 9 ? { amount: account?.accountOptions?.[262] - 9 } : {})
+    ...(index === 9 ? { amount: account?.accountOptions?.[262] - 10 } : {})
   }));
   return {
     upgrades,
