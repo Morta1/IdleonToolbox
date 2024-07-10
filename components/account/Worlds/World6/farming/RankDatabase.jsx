b3e94c258f8041e70f8227e04ba5cd18a7f8226c
Mor <mortaob@gmail.com>
2024-07-10 22:15:07 +0300

    fix: rank instead of lv

--- components/account/Worlds/World6/farming/RankDatabase.jsx
+++ components/account/Worlds/World6/farming/RankDatabase.jsx
@@ -5,18 +5,17 @@ import React from 'react';
 const RankDatabase = ({ ranks }) => {
   return <>
     <Stack direction={'row'} gap={1} flexWrap={'wrap'}>
-      {ranks?.map(({ name, upgradeLevel, description, bonus, progress, requirement, unlockAt }, index) => {
+      {ranks?.map(({ rank, name, upgradeLevel, description, bonus, progress, requirement, unlockAt }, index) => {
         const filter = 99 < upgradeLevel || 0 === (index + 1) % 5 || (24 < upgradeLevel
           ? 'grayscale(1)'
           : .5 < upgradeLevel && 'hue-rotate(330deg)')
-        console.log(filter)
         return <Card key={'rank-' + index} sx={{ width: 300, opacity: upgradeLevel === 0 ? .5 : 1 }}>
           <CardContent>
             <Stack direction={'row'} alignItems={'center'} gap={2}>
               <img src={`${prefix}data/RankUpg${index}.png`} style={{ filter }} alt={''}/>
               <Stack>
                 <Typography variant={'body1'}>{cleanUnderscore(name)}</Typography>
-                <Typography variant={'body1'}>Lv. {upgradeLevel}</Typography>
+                <Typography variant={'body1'}>Rank {rank}</Typography>
                 {upgradeLevel > 0 ? <Typography
                   variant={'body1'}>{notateNumber(progress)} / {notateNumber(requirement)}</Typography> : <Typography>
                   Unlocks at {unlockAt}
