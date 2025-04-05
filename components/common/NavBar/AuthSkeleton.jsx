f9e337977ec10df5151e6f906e427343de03f205
Mor <mortaob@gmail.com>
2025-04-05 13:29:20 +0300

    fix: margin

--- components/common/NavBar/AuthSkeleton.jsx
+++ components/common/NavBar/AuthSkeleton.jsx
@@ -3,11 +3,11 @@ import { Box, Skeleton, Stack } from '@mui/material';
 
 const AuthSkeleton = () => {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
       <Stack direction="row" spacing={1.5} alignItems="center">
         
         {/* User avatar skeleton */}
-        <Skeleton variant="circular" width={32} height={32} sx={{ bgcolor: 'rgba(255, 255, 255, 0.1)' }} />
+        <Skeleton  variant="circular" width={32} height={32} sx={{ bgcolor: 'rgba(255, 255, 255, 0.1)' }} />
         
         {/* Name and time skeleton */}
         <Stack>
