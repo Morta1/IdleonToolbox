a9efa8796cfc3135c88167185b560e83e324d89c
Mor <mortaob@gmail.com>
2025-04-27 21:43:50 +0300

    feat: sampling companion

--- components/common/AutoGrid.jsx
+++ components/common/AutoGrid.jsx
@@ -1,12 +1,19 @@
 import { Box } from '@mui/material';
 
-const AutoGrid = ({ children }) => {
+const AutoGrid = ({ children, withBorder }) => {
   return <Box sx={{
     display: 'grid',
     '--auto-grid-min-size': '18rem',
     gridTemplateColumns: 'repeat(auto-fill, minmax(var(--auto-grid-min-size), 1fr))',
     alignSelf: 'flex-start',
-    gap: '1rem'
+    gap: '1rem',
+    ...(withBorder && {
+      '& > *': {
+        border: '1px solid rgba(255, 255, 255, 0.12)',
+        borderRadius: '8px',
+        padding: '1rem'
+      }
+    })
   }}>
     {children}
   </Box>;
