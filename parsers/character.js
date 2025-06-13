c5cfb606c80ee248da5fbe72db2aac1e88203192
Mor <mortaob@gmail.com>
2025-06-13 20:53:38 +0300

    fix: apoc

--- parsers/character.js
+++ parsers/character.js
@@ -1327,7 +1327,7 @@ export const getBarbarianZowChow = (allKills, thresholds) => {
     'Grandfrog\'s_Backyard', 'Gravel_Tomb', 'Heaty_Hole',
     'Igloo\'s_Basement', 'Inside_the_Igloo', 'End_Of_The_Road',
     'Efaunt\'s_Tomb', 'Eycicles\'s_Nest', 'Enclave_a_la_Troll',
-    'Chizoar\'s_Cavern', 'KattleKruk\'s_Volcano', 'Castle_Interior'].toSimpleObject();
+    'Chizoar\'s_Cavern', 'KattleKruk\'s_Volcano', 'Castle_Interior', 'Emperor\'s_Castle'].toSimpleObject();
   const list = Object.values(mapNames).map((mapName, index) => {
     const rawName = mapEnemiesArray?.[index];
     const { MonsterFace, Name, AFKtype } = monsters?.[rawName] || {};
