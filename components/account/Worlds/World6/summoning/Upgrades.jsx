c689d8c338ecb4ac6840ca50711dbdfee2303058
Mor <mortaob@gmail.com>
2025-07-02 19:15:43 +0300

    feat: add view mode and sortByCost for summing upgrades

--- components/account/Worlds/World6/summoning/Upgrades.jsx
+++ components/account/Worlds/World6/summoning/Upgrades.jsx
@@ -1,6 +1,20 @@
-import { Box, Card, CardContent, Checkbox, FormControlLabel, Stack, ToggleButton, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  Checkbox,
+  FormControlLabel,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Tooltip,
+  Typography
+} from '@mui/material';
 import { cleanUnderscore, notateNumber, prefix } from '@utility/helpers';
 import React, { useMemo, useState } from 'react';
+import { useLocalStorage } from '@mantine/hooks';
+import { IconChartCohort, IconTable } from '@tabler/icons-react';
+
 
 const COLOR_MAP = {
   0: 'White',
@@ -11,41 +25,63 @@ const COLOR_MAP = {
   5: 'Red',
   6: 'Cyan'
 }
-const Upgrades = ({ upgrades, totalUpgradesLevels }) => {
-  const [sortBy, setSortBy] = useState(false);
-  const sortedByCost = useMemo(() => Object.values(upgrades).flat().filter(({
-                                                                              level,
-                                                                              maxLvl
-                                                                            }) => level < maxLvl).sort((a, b) => a.totalCost - b.totalCost), [upgrades]);
+const Upgrades = ({ upgrades, totalUpgradesLevels, resourceKey = prefix }) => {
+  const [sortByCost, setSortByCost] = useLocalStorage({
+    key: `${prefix}:summoningUpgrades:sortByCost`,
+    defaultValue: false
+  });  const [viewMode, setViewMode] = useLocalStorage({
+    key: `${prefix}:summoningUpgrades:viewMode`,
+    defaultValue: 'group'
+  });
+
+  const listForm = useMemo(() => Object.values(upgrades).flat(), [upgrades]);
+  const getSortedList = (list) => {
+    return list.filter(({
+                          level,
+                          maxLvl
+                        }) => level < maxLvl).sort((a, b) => a.totalCost - b.totalCost)
+  };
 
   return <Stack>
     <Stack mb={3} direction={'row'} alignItems={'center'}>
-      <ToggleButton sx={{ mr: 2, '&:disabled': { color: '#FFFFFF' } }} value={'maxLevel'} disabled>Total Upgrades: {totalUpgradesLevels}</ToggleButton>
+      <ToggleButton sx={{ mr: 2, '&:disabled': { color: '#FFFFFF' } }} value={'maxLevel'} disabled>Total
+        Upgrades: {totalUpgradesLevels}</ToggleButton>
       <FormControlLabel
         sx={{ width: 'fit-content' }}
-        control={<Checkbox name={'mini'} checked={sortBy}
+        control={<Checkbox name={'mini'} checked={sortByCost}
                            size={'small'}
-                           onChange={() => setSortBy(!sortBy)}/>}
+                           onChange={() => setSortByCost(!sortByCost)}/>}
         label={'Sort by cost'}/>
+      <ToggleButtonGroup
+        value={viewMode}
+        exclusive
+        sx={{ ml: 'auto' }}
+        onChange={(_, val) => val && setViewMode(val)}
+      >
+        <Tooltip title={'Group view'}><ToggleButton sx={{ height: 40 }}
+                                                    value="group"><IconChartCohort/></ToggleButton></Tooltip>
+        <Tooltip title={'Grid view'}><ToggleButton sx={{ height: 40 }}
+                                                   value="grid"><IconTable/></ToggleButton></Tooltip>
+      </ToggleButtonGroup>
     </Stack>
-    {sortBy ? <Box sx={{
+    {viewMode === 'grid' ? <Box sx={{
         display: 'grid',
         '--auto-grid-min-size': '16rem',
         gridTemplateColumns: 'repeat(auto-fill, minmax(var(--auto-grid-min-size), 1fr))',
         gap: '1rem'
       }}>
-        {sortedByCost.map(({
-                             level,
-                             value,
-                             maxLvl,
-                             name,
-                             bonus,
-                             totalBonus,
-                             doubled,
-                             totalCost,
-                             originalIndex,
-                             colour
-                           }, index) => {
+        {(sortByCost ? getSortedList(listForm) : listForm).map(({
+                                                              level,
+                                                              value,
+                                                              maxLvl,
+                                                              name,
+                                                              bonus,
+                                                              totalBonus,
+                                                              doubled,
+                                                              totalCost,
+                                                              originalIndex,
+                                                              colour
+                                                            }, index) => {
           const unlocked = (level > 0) || (colour === '0' && originalIndex === 2);
           return <Card key={'upgrade-' + index} sx={{ opacity: unlocked ? 1 : .5 }}>
             <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
@@ -66,20 +102,21 @@ const Upgrades = ({ upgrades, totalUpgradesLevels }) => {
         })}
       </Box> :
       <>{Object.entries(upgrades || {})?.map(([colorIndex, colorUpgrades]) => {
+        const currentList = sortByCost ? getSortedList(colorUpgrades) : colorUpgrades;
         return COLOR_MAP[colorIndex] ? <Stack key={'color-upgrade-' + colorIndex}>
           <Typography variant={'h5'}>{COLOR_MAP[colorIndex]}</Typography>
           <Stack direction={'row'} flexWrap={'wrap'} gap={1} my={1}>
-            {colorUpgrades.map(({
-                                  level,
-                                  value,
-                                  maxLvl,
-                                  name,
-                                  bonus,
-                                  totalBonus,
-                                  doubled,
-                                  totalCost,
-                                  originalIndex
-                                }, index) => {
+            {currentList.map(({
+                                level,
+                                value,
+                                maxLvl,
+                                name,
+                                bonus,
+                                totalBonus,
+                                doubled,
+                                totalCost,
+                                originalIndex
+                              }, index) => {
               const unlocked = (level > 0) || (colorIndex === '0' && originalIndex === 2);
               return <Card key={'upgrade-' + index} sx={{ width: 220, opacity: unlocked ? 1 : .5 }}>
                 <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
