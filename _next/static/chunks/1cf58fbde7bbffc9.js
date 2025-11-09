c7dd493791cda92b5ae350021f47e4038285cf1f
Morta1 <Morta1@users.noreply.github.com>
2025-11-09 11:59:48 +0000

    deploy: 06ce4a75efec727e8f5766dad62df0b0185f3977

---
+++ _next/static/chunks/1cf58fbde7bbffc9.js
@@ -0,0 +1,17 @@
+__turbopack_load_page_chunks__("/account/prem-currency/companions", [
+  "static/chunks/9afd01cf6cc5fb6e.js",
+  "static/chunks/7a88b545e6c2aae9.js",
+  "static/chunks/6721cb039483193d.js",
+  "static/chunks/cbc67c9543d2016a.js",
+  "static/chunks/1079be68ff366a94.js",
+  "static/chunks/cc01ccd93f32e7f5.js",
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/9db5c5fd1e85123b.js",
+  "static/chunks/dc1efa255576d82e.js",
+  "static/chunks/20d540ed6a2c42e6.js",
+  "static/chunks/403c49ca47413a4e.js",
+  "static/chunks/18d6a4df0a7cdd5b.js",
+  "static/chunks/a4df4b21675e4c4b.js",
+  "static/chunks/ad01d767f11a010d.js",
+  "static/chunks/turbopack-f7cde45f0bccf0a2.js"
+])
---
+++ _next/static/chunks/f4d106e40665fc52.js
@@ -0,0 +1,19 @@
+__turbopack_load_page_chunks__("/account/world-3/construction", [
+  "static/chunks/6721cb039483193d.js",
+  "static/chunks/1079be68ff366a94.js",
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/35ca0f1f1894ff36.js",
+  "static/chunks/a4df4b21675e4c4b.js",
+  "static/chunks/7a88b545e6c2aae9.js",
+  "static/chunks/f38b21d745d1bca2.js",
+  "static/chunks/cbc67c9543d2016a.js",
+  "static/chunks/ad01d767f11a010d.js",
+  "static/chunks/9db5c5fd1e85123b.js",
+  "static/chunks/403c49ca47413a4e.js",
+  "static/chunks/7f943b11694134b7.js",
+  "static/chunks/f0b03e6e334a00b1.js",
+  "static/chunks/18d6a4df0a7cdd5b.js",
+  "static/chunks/faf03599252c1a70.js",
+  "static/chunks/dc1efa255576d82e.js",
+  "static/chunks/turbopack-57a179e8c5d73606.js"
+])
