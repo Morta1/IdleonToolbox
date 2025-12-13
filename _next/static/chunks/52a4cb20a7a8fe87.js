228ac797c01d662cbb319ae5f88abb39d47c923d
Morta1 <Morta1@users.noreply.github.com>
2025-12-13 09:53:41 +0000

    deploy: 6b3eb44c2fb268c3eb1d4fb0b147320f214230c5

---
+++ _next/static/chunks/52a4cb20a7a8fe87.js
@@ -0,0 +1,18 @@
+__turbopack_load_page_chunks__("/account/world-2/sigils", [
+  "static/chunks/0083eefc474d600f.js",
+  "static/chunks/1079be68ff366a94.js",
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/a0f10a051705e44b.js",
+  "static/chunks/f168635ae1077fb5.js",
+  "static/chunks/68994b17aeab8e32.js",
+  "static/chunks/f38b21d745d1bca2.js",
+  "static/chunks/dc1efa255576d82e.js",
+  "static/chunks/d0ca254f9a70f56f.js",
+  "static/chunks/ef054655485a17fd.js",
+  "static/chunks/403c49ca47413a4e.js",
+  "static/chunks/e59e0c983f908c2c.js",
+  "static/chunks/65861a372e6fb9f2.js",
+  "static/chunks/cbc67c9543d2016a.js",
+  "static/chunks/ee3ffbdad88b6edb.js",
+  "static/chunks/turbopack-b12dc3fb1cf39afe.js"
+])
