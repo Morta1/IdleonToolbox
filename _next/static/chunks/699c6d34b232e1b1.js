b16dd548b032d3b027df4116126b905946ee0a89
Morta1 <Morta1@users.noreply.github.com>
2025-12-23 10:23:05 +0000

    deploy: df664ff837965266e05ba5b2d4434abaeec1f2b3

---
+++ _next/static/chunks/699c6d34b232e1b1.js
@@ -0,0 +1,14 @@
+__turbopack_load_page_chunks__("/statistics", [
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/1cc82b66428b1a8d.js",
+  "static/chunks/59b49cb897240a65.js",
+  "static/chunks/9e118b4d72a2d815.js",
+  "static/chunks/d18449e55d5a554a.js",
+  "static/chunks/9f81dd03354ff662.js",
+  "static/chunks/4054dd592f66bdda.js",
+  "static/chunks/24f37a0f6bb28722.js",
+  "static/chunks/49d78ef4063457ff.js",
+  "static/chunks/b9898908ac97cd52.js",
+  "static/chunks/1079be68ff366a94.js",
+  "static/chunks/turbopack-d1a3edefd5c1a378.js"
+])
---
+++ _next/static/chunks/d63fe2f9141a2b3d.js
@@ -0,0 +1,18 @@
+__turbopack_load_page_chunks__("/account/world-2/cauldrons", [
+  "static/chunks/14af71244d9d9664.js",
+  "static/chunks/1079be68ff366a94.js",
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/9e118b4d72a2d815.js",
+  "static/chunks/65861a372e6fb9f2.js",
+  "static/chunks/68994b17aeab8e32.js",
+  "static/chunks/5b4cd1c5f94dfe5a.js",
+  "static/chunks/24280f67c347b030.js",
+  "static/chunks/ef054655485a17fd.js",
+  "static/chunks/b9898908ac97cd52.js",
+  "static/chunks/cbc67c9543d2016a.js",
+  "static/chunks/4054dd592f66bdda.js",
+  "static/chunks/49d78ef4063457ff.js",
+  "static/chunks/e9d479a523b98b1d.js",
+  "static/chunks/cc01ccd93f32e7f5.js",
+  "static/chunks/turbopack-3de31748218fad28.js"
+])
