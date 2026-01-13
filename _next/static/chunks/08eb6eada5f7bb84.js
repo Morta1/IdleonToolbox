c3121847392ee8c7722b628565dde73122507890
Morta1 <Morta1@users.noreply.github.com>
2026-01-13 11:38:08 +0000

    deploy: 54658a97902fd5135b13b4b7a79542ca19dbb854

---
+++ _next/static/chunks/08eb6eada5f7bb84.js
@@ -0,0 +1,18 @@
+__turbopack_load_page_chunks__("/account/misc/dungeons", [
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/c23ac14a51fc36a7.js",
+  "static/chunks/3371a29f55d15482.js",
+  "static/chunks/c652536170c8f688.js",
+  "static/chunks/cf026bc248fcac7e.js",
+  "static/chunks/7010034fcbea6d9f.js",
+  "static/chunks/32fef4392fa34b20.js",
+  "static/chunks/44206aa42820f88c.js",
+  "static/chunks/ef054655485a17fd.js",
+  "static/chunks/cbc67c9543d2016a.js",
+  "static/chunks/dadc17446b449b02.js",
+  "static/chunks/b9898908ac97cd52.js",
+  "static/chunks/49d78ef4063457ff.js",
+  "static/chunks/b71905f4c7ec0afc.js",
+  "static/chunks/09fe48ee17f66159.js",
+  "static/chunks/turbopack-439cc70158c70e0a.js"
+])
---
+++ _next/static/chunks/cbb64aa609f5da2d.js
@@ -0,0 +1,9 @@
+__turbopack_load_page_chunks__("/patch-notes", [
+  "static/chunks/c23ac14a51fc36a7.js",
+  "static/chunks/861fc929dc2fcda7.js",
+  "static/chunks/347af9508aca8bb4.js",
+  "static/chunks/b9898908ac97cd52.js",
+  "static/chunks/b71905f4c7ec0afc.js",
+  "static/chunks/49d78ef4063457ff.js",
+  "static/chunks/turbopack-b5e6a3583a690774.js"
+])
