ddaacb636aecf8bfbed68e0a2e173cf59842ae13
Morta1 <Morta1@users.noreply.github.com>
2026-03-02 19:56:37 +0000

    deploy: a3af33bb15a58b1255dc1f0e184ab9bcc985aebd

--- _next/static/chunks/22d4b17427776d0d.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/patch-notes", [
-  "static/chunks/5999249f67defeab.js",
-  "static/chunks/d4a3ff9246c94bc7.js",
-  "static/chunks/347af9508aca8bb4.js",
-  "static/chunks/b9898908ac97cd52.js",
-  "static/chunks/b71905f4c7ec0afc.js",
-  "static/chunks/49d78ef4063457ff.js",
-  "static/chunks/turbopack-057d2b7274a53595.js"
-])
