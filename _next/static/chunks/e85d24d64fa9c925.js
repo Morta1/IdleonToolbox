3c85db084ea8d9f83576ba403af70cd138a6d50d
Morta1 <Morta1@users.noreply.github.com>
2026-03-03 19:44:44 +0000

    deploy: d43fda9023d0c768aad1a79a0d550d877b926d55

---
+++ _next/static/chunks/e85d24d64fa9c925.js
@@ -0,0 +1,16 @@
+__turbopack_load_page_chunks__("/account/misc/constellations", [
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/14c1dbfa59234543.js",
+  "static/chunks/907e8bb0101b99ed.js",
+  "static/chunks/49d78ef4063457ff.js",
+  "static/chunks/d6f60857580fa042.js",
+  "static/chunks/3a9d47a3f3f3bd4f.js",
+  "static/chunks/bcc2056cbb1dca1f.js",
+  "static/chunks/1d284420bded0a56.js",
+  "static/chunks/6a59ded399840b3a.js",
+  "static/chunks/0b3e6744ef4aea57.js",
+  "static/chunks/6056e4b647aed9c6.js",
+  "static/chunks/b9898908ac97cd52.js",
+  "static/chunks/a0f10a051705e44b.js",
+  "static/chunks/turbopack-d4253bfcb52d8057.js"
+])
