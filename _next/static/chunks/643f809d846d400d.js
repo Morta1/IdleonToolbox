c139c03a8ba2bf5493b58adae21f5434fb046d08
Morta1 <Morta1@users.noreply.github.com>
2026-03-11 14:59:36 +0000

    deploy: 0ccd2e83c177c17945644e68faa542bc55fee9ef

---
+++ _next/static/chunks/643f809d846d400d.js
@@ -0,0 +1,17 @@
+__turbopack_load_page_chunks__("/account/misc/friends-stats", [
+  "static/chunks/c6df4ae7b6059e11.js",
+  "static/chunks/708dc2c8f3dac909.js",
+  "static/chunks/4d9770a92ec413f3.js",
+  "static/chunks/0b3e6744ef4aea57.js",
+  "static/chunks/3e19b5ff68115210.js",
+  "static/chunks/aa64c19f518e4e57.js",
+  "static/chunks/32fef4392fa34b20.js",
+  "static/chunks/9c224590621d86b6.js",
+  "static/chunks/bcc2056cbb1dca1f.js",
+  "static/chunks/b9898908ac97cd52.js",
+  "static/chunks/e9d479a523b98b1d.js",
+  "static/chunks/49d78ef4063457ff.js",
+  "static/chunks/22bd56094e01b68a.js",
+  "static/chunks/bef54f861694aa38.js",
+  "static/chunks/turbopack-bc3b41e81aec42b1.js"
+])
