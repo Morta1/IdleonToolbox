25d3d9a18310710778f3d5b09bf6b4dd5d986d9f
Mor <mortaob@gmail.com>
2026-03-13 23:04:42 +0200

    feat: 2.3.494

--- utility/lavaRand.js
+++ utility/lavaRand.js
@@ -1,22 +1,22 @@
 let Bf = {}
 Bf._mul = Math.imul;
 const z = {}
-var dc = function(e) {
+var cc = function(e) {
   this.init(e);
 };
-((z.Rand = dc),
-  (dc.__name__ = "Rand"),
-  (dc.hash = function(e, t) {
+((z.Rand = cc),
+  (cc.__name__ = "Rand"),
+  (cc.hash = function(e, t) {
     return (null == t && (t = 5381), (e = Bf._mul(e, -862048943)), (t ^= e = Bf._mul((e << 15) | (e >>> 17), 461845907)), (t = (Bf._mul((t << 13) | (t >>> 19), 5) + -430675100) | 0), (t = Bf._mul(t ^ (t >> 16), -2048144789)), (t = Bf._mul(t ^ (t >> 13), -1028477387)) ^ (t >> 16));
   }),
-  (dc.inlineHash = function(e, t) {
+  (cc.inlineHash = function(e, t) {
     return ((e = Bf._mul(e, -862048943)), (t ^= e = Bf._mul((e << 15) | (e >>> 17), 461845907)), (t = (Bf._mul((t << 13) | (t >>> 19), 5) + -430675100) | 0), (t = Bf._mul(t ^ (t >> 16), -2048144789)), (t = Bf._mul(t ^ (t >> 13), -1028477387)) ^ (t >> 16));
   }),
-  (dc.prototype = {
+  (cc.prototype = {
     seed: null,
     seed2: null,
     init: function(e) {
-      ((this.seed = e), (this.seed2 = dc.hash(e)), 0 == this.seed && (this.seed = 1), 0 == this.seed2 && (this.seed2 = 1));
+      ((this.seed = e), (this.seed2 = cc.hash(e)), 0 == this.seed && (this.seed = 1), 0 == this.seed2 && (this.seed2 = 1));
     },
     random: function(e) {
       return ((this.seed = 36969 * (65535 & this.seed) + (this.seed >> 16)), (this.seed2 = 18e3 * (65535 & this.seed2) + (this.seed2 >> 16)), (1073741823 & (((this.seed << 16) + this.seed2) | 0)) % e);
@@ -43,6 +43,6 @@ var dc = function(e) {
     uint: function() {
       return ((this.seed = 36969 * (65535 & this.seed) + (this.seed >> 16)), (this.seed2 = 18e3 * (65535 & this.seed2) + (this.seed2 >> 16)), 1073741823 & (((this.seed << 16) + this.seed2) | 0));
     },
-    __class__: dc
+    __class__: cc
   }));
-export default dc;
+export default cc;
