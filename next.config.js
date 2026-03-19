e2aa2dbdf1ea8d487e4b200c31e8ef5f53f7990b
Mor <mortaob@gmail.com>
2026-03-19 10:05:38 +0200

    chore: improvements

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const { withSentryConfig } = require("@sentry/nextjs");
-
-const baseConfig = {
+module.exports = {
   reactStrictMode: false,
   assetPrefix: '/',
   output: 'export',
@@ -9,32 +7,3 @@ const baseConfig = {
     unoptimized: true,
   },
 };
-
-const sentryConfig = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: "idleon-toolbox",
-  project: "javascript-nextjs",
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  widenClientFileUpload: true,
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // tunnelRoute: "/monitoring",
-
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-};
-
-module.exports = baseConfig;
