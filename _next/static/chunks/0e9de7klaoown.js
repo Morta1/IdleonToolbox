dec147c5cfc24c2187f6c80ac406dde58d82e601
Morta1 <Morta1@users.noreply.github.com>
2026-03-31 14:27:42 +0000

    deploy: 52f0a6333f7e28f995739b9695efa62332889b36

---
+++ _next/static/chunks/0e9de7klaoown.js
@@ -0,0 +1,18 @@
+__turbopack_load_page_chunks__("/account/world-3/worship", [
+  "static/chunks/0uijkgee292im.js",
+  "static/chunks/01zlgdf038qjm.js",
+  "static/chunks/04vp_q3b86fcg.js",
+  "static/chunks/0gdhjfdy-wuqx.js",
+  "static/chunks/0hha4~5yi~g34.js",
+  "static/chunks/0ey67gvc7ejl0.js",
+  "static/chunks/01ml-prlki4hf.js",
+  "static/chunks/0ezjpl3ndth8w.js",
+  "static/chunks/0597wbtrfdytl.js",
+  "static/chunks/0b7rnuxo0jn_w.js",
+  "static/chunks/0blgt86iauoef.js",
+  "static/chunks/13z1bhslfki8l.js",
+  "static/chunks/0ml-r6_q-zu7..js",
+  "static/chunks/0otdh-5fu63-z.js",
+  "static/chunks/10k14qbn65enz.js",
+  "static/chunks/turbopack-0ah_y.taq61yx.js"
+])
