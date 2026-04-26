9126001bcb915aab0147daa16bf48ce119f88574
Morta1 <Morta1@users.noreply.github.com>
2026-04-26 18:45:46 +0000

    deploy: 267427026a8ba59dc1077196268b4a9ec4f5663e

---
+++ _next/static/PUOXex6JW4vWDvvAiY4yH/_buildManifest.js
@@ -0,0 +1,434 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0l~t~9bvtu35p.js"
+  ],
+  "/404": [
+    "static/chunks/12864djro.oa9.js"
+  ],
+  "/_error": [
+    "static/chunks/00~30hr7~j148.js"
+  ],
+  "/account/class-specific/apocalypses": [
+    "static/chunks/0e9-8nxlvc73y.js"
+  ],
+  "/account/class-specific/compass": [
+    "static/chunks/1336mn64nnmuz.js"
+  ],
+  "/account/class-specific/grimoire": [
+    "static/chunks/0yd1f6.ysg9-q.js"
+  ],
+  "/account/class-specific/tesseract": [
+    "static/chunks/03oxce6aw42nr.js"
+  ],
+  "/account/clickers/bubba": [
+    "static/chunks/0a4yuq28bdtf0.js"
+  ],
+  "/account/clickers/kangaroo": [
+    "static/chunks/0rwvdycob7zad.js"
+  ],
+  "/account/clickers/owl": [
+    "static/chunks/02fkobcjsv-4g.js"
+  ],
+  "/account/misc/constellations": [
+    "static/chunks/0orr8jc8s1axp.js"
+  ],
+  "/account/misc/dungeons": [
+    "static/chunks/0cq22ns1wzwga.js"
+  ],
+  "/account/misc/event-shop": [
+    "static/chunks/06_tt2gw2a28a.js"
+  ],
+  "/account/misc/friends-stats": [
+    "static/chunks/0vdepoo7aaeiv.js"
+  ],
+  "/account/misc/general": [
+    "static/chunks/0vq8jnaxlqfmg.js"
+  ],
+  "/account/misc/guild": [
+    "static/chunks/0fjdboqazwcay.js"
+  ],
+  "/account/misc/quests": [
+    "static/chunks/0l92xz-hkcrh2.js"
+  ],
+  "/account/misc/random-events": [
+    "static/chunks/03u0b6bi8b1h_.js"
+  ],
+  "/account/misc/storage": [
+    "static/chunks/0uq5os9e~_-g..js"
+  ],
+  "/account/misc/tournament": [
+    "static/chunks/0m2-1e9643r~f.js"
+  ],
+  "/account/misc/upgrade-vault": [
+    "static/chunks/0fxcbp36p8~3j.js"
+  ],
+  "/account/prem-currency/bundles": [
+    "static/chunks/0fdyrpw_s._u3.js"
+  ],
+  "/account/prem-currency/gem-shop": [
+    "static/chunks/0h0vsarr.ykrh.js"
+  ],
+  "/account/prem-currency/pets": [
+    "static/chunks/0.szc-iiaktew.js"
+  ],
+  "/account/task-board/achievements": [
+    "static/chunks/0-9841mmwuj8b.js"
+  ],
+  "/account/task-board/merits": [
+    "static/chunks/0t~m26phnb~eb.js"
+  ],
+  "/account/task-board/tasks": [
+    "static/chunks/10djki_zqh_3w.js"
+  ],
+  "/account/task-board/unlocks": [
+    "static/chunks/0cv2n3g_a4wyo.js"
+  ],
+  "/account/world-1/anvil": [
+    "static/chunks/0o1gwqkuekcr~.js"
+  ],
+  "/account/world-1/bribes": [
+    "static/chunks/06~udc-~ppt53.js"
+  ],
+  "/account/world-1/forge": [
+    "static/chunks/0ut5w58l2pf0t.js"
+  ],
+  "/account/world-1/stamps": [
+    "static/chunks/092d1zn5.hbo~.js"
+  ],
+  "/account/world-2/arcade-shop": [
+    "static/chunks/14bl20niguqzh.js"
+  ],
+  "/account/world-2/bubbles": [
+    "static/chunks/0-_onruo_lp_6.js"
+  ],
+  "/account/world-2/cauldrons": [
+    "static/chunks/17amlvai0u.8a.js"
+  ],
+  "/account/world-2/islands": [
+    "static/chunks/0t1wjlxh9v9i-.js"
+  ],
+  "/account/world-2/killroy": [
+    "static/chunks/1399hgzkhf_~5.js"
+  ],
+  "/account/world-2/sigils": [
+    "static/chunks/16jp~2m8ulf63.js"
+  ],
+  "/account/world-2/vials": [
+    "static/chunks/0vjm.n4a6s0m~.js"
+  ],
+  "/account/world-2/vote-ballot": [
+    "static/chunks/0ip94t3nanlag.js"
+  ],
+  "/account/world-2/weekly-bosses": [
+    "static/chunks/0v955jpl4fiu8.js"
+  ],
+  "/account/world-3/armor-smithy": [
+    "static/chunks/0mh_rufmye28-.js"
+  ],
+  "/account/world-3/atom-collider": [
+    "static/chunks/02~_-_.e_fd4k.js"
+  ],
+  "/account/world-3/buildings": [
+    "static/chunks/072r1ty02a1kv.js"
+  ],
+  "/account/world-3/construction": [
+    "static/chunks/0gpynbjnpnxse.js"
+  ],
+  "/account/world-3/death-note": [
+    "static/chunks/11~d4.fq718g4.js"
+  ],
+  "/account/world-3/equinox": [
+    "static/chunks/0bi2e45rnqlr~.js"
+  ],
+  "/account/world-3/hat-rack": [
+    "static/chunks/0hexruk9-cpcg.js"
+  ],
+  "/account/world-3/prayers": [
+    "static/chunks/0v9hgw1hvaqsh.js"
+  ],
+  "/account/world-3/printer": [
+    "static/chunks/0ocaz.rszfz5_.js"
+  ],
+  "/account/world-3/refinery": [
+    "static/chunks/07d_-f.ykjhx6.js"
+  ],
+  "/account/world-3/salt-lick": [
+    "static/chunks/0aq9-g52_-~g-.js"
+  ],
+  "/account/world-3/traps": [
+    "static/chunks/0r_myts1kuc5t.js"
+  ],
+  "/account/world-3/worship": [
+    "static/chunks/0-j8uit0jext5.js"
+  ],
+  "/account/world-4/breeding": [
+    "static/chunks/0_6o.drrcplb7.js"
+  ],
+  "/account/world-4/cooking": [
+    "static/chunks/06g-st6ocp4ri.js"
+  ],
+  "/account/world-4/laboratory": [
+    "static/chunks/03galu3_cgb77.js"
+  ],
+  "/account/world-4/rift": [
+    "static/chunks/00v74fl41ql.0.js"
+  ],
+  "/account/world-4/tome": [
+    "static/chunks/0c2fz9be.louc.js"
+  ],
+  "/account/world-5/divinity": [
+    "static/chunks/0qbc8erdqq4d7.js"
+  ],
+  "/account/world-5/gaming": [
+    "static/chunks/12wm8xz9p270y.js"
+  ],
+  "/account/world-5/hole": [
+    "static/chunks/0kgs0z-8fkz81.js"
+  ],
+  "/account/world-5/sailing": [
+    "static/chunks/06ym093ua1qdm.js"
+  ],
+  "/account/world-5/slab": [
+    "static/chunks/0n7k~lj1cf8eg.js"
+  ],
+  "/account/world-6/beanstalk": [
+    "static/chunks/172om.weedzch.js"
+  ],
+  "/account/world-6/emperor": [
+    "static/chunks/0k1_2o3f667ou.js"
+  ],
+  "/account/world-6/farming": [
+    "static/chunks/0~n1rf6_no_of.js"
+  ],
+  "/account/world-6/sneaking": [
+    "static/chunks/0zf4bvk.m6hwf.js"
+  ],
+  "/account/world-6/summoning": [
+    "static/chunks/0e~zask9tegux.js"
+  ],
+  "/account/world-7/advice-fish": [
+    "static/chunks/0nq~0rshh_r52.js"
+  ],
+  "/account/world-7/clam-work": [
+    "static/chunks/17cyw-1fc33w2.js"
+  ],
+  "/account/world-7/coral-reef": [
+    "static/chunks/0cqtu1y~-~j0d.js"
+  ],
+  "/account/world-7/gallery": [
+    "static/chunks/09ox5qz57jbr4.js"
+  ],
+  "/account/world-7/glimbo": [
+    "static/chunks/0_~z39r2hf6.7.js"
+  ],
+  "/account/world-7/legend-talents": [
+    "static/chunks/13tpn8gnr0-mt.js"
+  ],
+  "/account/world-7/minehead": [
+    "static/chunks/0h59gbol065f1.js"
+  ],
+  "/account/world-7/research": [
+    "static/chunks/0nk.yh_.c0_wm.js"
+  ],
+  "/account/world-7/s3": [
+    "static/chunks/0utrtwpyvotai.js"
+  ],
+  "/account/world-7/spelunking": [
+    "static/chunks/0xtopn~rb8.lc.js"
+  ],
+  "/account/world-7/sushi-station": [
+    "static/chunks/0ykko7exthu33.js"
+  ],
+  "/account/world-7/the-button": [
+    "static/chunks/0am2wynv-kk~_.js"
+  ],
+  "/account/world-7/zenith-market": [
+    "static/chunks/0xsza2-3fhmc~.js"
+  ],
+  "/characters": [
+    "static/chunks/0dszte.n0_u4~.js"
+  ],
+  "/dashboard": [
+    "static/chunks/0ail64c768ng5.js"
+  ],
+  "/guilds": [
+    "static/chunks/0wzkni-4rz.hb.js"
+  ],
+  "/leaderboards": [
+    "static/chunks/0i~84pfyv85yn.js"
+  ],
+  "/patch-notes": [
+    "static/chunks/0kt9e1.hb5ne_.js"
+  ],
+  "/settings": [
+    "static/chunks/02o8e3qdl62c2.js"
+  ],
+  "/statistics": [
+    "static/chunks/06bw1v.zp~bws.js"
+  ],
+  "/tools": [
+    "static/chunks/0e0r.knir6ln~.js"
+  ],
+  "/tools/active-exp-calculator": [
+    "static/chunks/0arw4usp_w1~c.js"
+  ],
+  "/tools/active-stuff-calculator": [
+    "static/chunks/0jf57kmq32lwf.js"
+  ],
+  "/tools/builds": [
+    "static/chunks/0l5baskjmga4l.js"
+  ],
+  "/tools/builds/edit": [
+    "static/chunks/0gcwuo8fcz0x7.js"
+  ],
+  "/tools/builds/my-builds": [
+    "static/chunks/11ublcx2o8axl.js"
+  ],
+  "/tools/builds/new": [
+    "static/chunks/09h8tvaib-lfq.js"
+  ],
+  "/tools/builds/view": [
+    "static/chunks/0ucn.nv2xee2c.js"
+  ],
+  "/tools/card-search": [
+    "static/chunks/0agdqikgclu7m.js"
+  ],
+  "/tools/formulas": [
+    "static/chunks/0uibtwb5regbp.js"
+  ],
+  "/tools/god-planner": [
+    "static/chunks/0zdm~bse.dcie.js"
+  ],
+  "/tools/guaranteed-drop-calculator": [
+    "static/chunks/0yazgqjdex10x.js"
+  ],
+  "/tools/item-browser": [
+    "static/chunks/0~u5_1e6_o-9c.js"
+  ],
+  "/tools/item-database": [
+    "static/chunks/0q51poq5rmz.b.js"
+  ],
+  "/tools/item-planner": [
+    "static/chunks/17pkyh25m3p8x.js"
+  ],
+  "/tools/material-tracker": [
+    "static/chunks/0o9e99jgx-2dg.js"
+  ],
+  "/tools/sampling-companion": [
+    "static/chunks/0i8ol-_a8s_ti.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/account/class-specific/apocalypses",
+    "/account/class-specific/compass",
+    "/account/class-specific/grimoire",
+    "/account/class-specific/tesseract",
+    "/account/clickers/bubba",
+    "/account/clickers/kangaroo",
+    "/account/clickers/owl",
+    "/account/misc/constellations",
+    "/account/misc/dungeons",
+    "/account/misc/event-shop",
+    "/account/misc/friends-stats",
+    "/account/misc/general",
+    "/account/misc/guild",
+    "/account/misc/quests",
+    "/account/misc/random-events",
+    "/account/misc/storage",
+    "/account/misc/tournament",
+    "/account/misc/upgrade-vault",
+    "/account/prem-currency/bundles",
+    "/account/prem-currency/gem-shop",
+    "/account/prem-currency/pets",
+    "/account/task-board/achievements",
+    "/account/task-board/merits",
+    "/account/task-board/tasks",
+    "/account/task-board/unlocks",
+    "/account/world-1/anvil",
+    "/account/world-1/bribes",
+    "/account/world-1/forge",
+    "/account/world-1/stamps",
+    "/account/world-2/arcade-shop",
+    "/account/world-2/bubbles",
+    "/account/world-2/cauldrons",
+    "/account/world-2/islands",
+    "/account/world-2/killroy",
+    "/account/world-2/sigils",
+    "/account/world-2/vials",
+    "/account/world-2/vote-ballot",
+    "/account/world-2/weekly-bosses",
+    "/account/world-3/armor-smithy",
+    "/account/world-3/atom-collider",
+    "/account/world-3/buildings",
+    "/account/world-3/construction",
+    "/account/world-3/death-note",
+    "/account/world-3/equinox",
+    "/account/world-3/hat-rack",
+    "/account/world-3/prayers",
+    "/account/world-3/printer",
+    "/account/world-3/refinery",
+    "/account/world-3/salt-lick",
+    "/account/world-3/traps",
+    "/account/world-3/worship",
+    "/account/world-4/breeding",
+    "/account/world-4/cooking",
+    "/account/world-4/laboratory",
+    "/account/world-4/rift",
+    "/account/world-4/tome",
+    "/account/world-5/divinity",
+    "/account/world-5/gaming",
+    "/account/world-5/hole",
+    "/account/world-5/sailing",
+    "/account/world-5/slab",
+    "/account/world-6/beanstalk",
+    "/account/world-6/emperor",
+    "/account/world-6/farming",
+    "/account/world-6/sneaking",
+    "/account/world-6/summoning",
+    "/account/world-7/advice-fish",
+    "/account/world-7/clam-work",
+    "/account/world-7/coral-reef",
+    "/account/world-7/gallery",
+    "/account/world-7/glimbo",
+    "/account/world-7/legend-talents",
+    "/account/world-7/minehead",
+    "/account/world-7/research",
+    "/account/world-7/s3",
+    "/account/world-7/spelunking",
+    "/account/world-7/sushi-station",
+    "/account/world-7/the-button",
+    "/account/world-7/zenith-market",
+    "/characters",
+    "/dashboard",
+    "/guilds",
+    "/leaderboards",
+    "/patch-notes",
+    "/settings",
+    "/statistics",
+    "/tools",
+    "/tools/active-exp-calculator",
+    "/tools/active-stuff-calculator",
+    "/tools/builds",
+    "/tools/builds/edit",
+    "/tools/builds/my-builds",
+    "/tools/builds/new",
+    "/tools/builds/view",
+    "/tools/card-search",
+    "/tools/formulas",
+    "/tools/god-planner",
+    "/tools/guaranteed-drop-calculator",
+    "/tools/item-browser",
+    "/tools/item-database",
+    "/tools/item-planner",
+    "/tools/material-tracker",
+    "/tools/sampling-companion"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0yazgqjdex10x.js
@@ -0,0 +1,14 @@
+__turbopack_load_page_chunks__("/tools/guaranteed-drop-calculator", [
+  "static/chunks/0wnwpn90qss6n.js",
+  "static/chunks/0vb1jf5sau__0.js",
+  "static/chunks/0hha4~5yi~g34.js",
+  "static/chunks/0dvtp~p-87by9.js",
+  "static/chunks/18b-jxwj8-z0y.js",
+  "static/chunks/0peqbuus1s-.y.js",
+  "static/chunks/0c57-1a-gy4nm.js",
+  "static/chunks/0otdh-5fu63-z.js",
+  "static/chunks/0gdhjfdy-wuqx.js",
+  "static/chunks/0se18crl4oic7.js",
+  "static/chunks/0-j_ewpm-0qqi.js",
+  "static/chunks/turbopack-15mqfa06ijwj6.js"
+])
